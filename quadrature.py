d88d9825a4c4a5d96e2f7d7b5b181ad566b43821
martinsw01 <martin.s.winther@gmail.com>
2023-03-15 17:22:56 +0100

    Use quadrature implemented in scipy

--- quadrature.py
+++ quadrature.py
@@ -1,12 +1,12 @@
+from scipy.integrate import quad  # Incredibly overkill?
+
 def quadrature_phi0(f, x0, x1) -> float:
-    """
-    Integrate f * phi0 on the element K=(x0, x1)
-    """
-    pass
+    def f_psi0(x):
+        return f(x) * (x-x0)/(x1-x0)
+    return quad(f_psi0, x0, x1)[0]
 
 
 def quadrature_phi1(f, x0, x1) -> float:
-    """
-    Integrate f * phi1 on the element K=(x0, x1)
-    """
-    pass
+    def f_psi1(x):
+        return f(x) * (x-x1)/(x0-x1)
+    return quad(f_psi1, x0, x1)[0]

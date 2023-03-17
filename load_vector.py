8399fce49cbabf2a598b580f0cfee1f1b49aee99
martinsw01 <martin.s.winther@gmail.com>
2023-03-17 15:19:27 +0100

    Solve eq only for interior nodes. Add boundary condition to load vector.

--- load_vector.py
+++ load_vector.py
@@ -10,15 +10,18 @@ def elemental_load_vector(f, x0, x1):
     ])
 
 
-def load_vector(f, X, M):
+def load_vector(f, X, M, g0=0, g1=0):
     """
     :param f: Source
     :param X: Grid points from 0 to 1 of size M
+    :param g0: boundary at x=0
+    :param g1: boundary at x=1
     """
 
-    F = np.zeros_like(X)
-    for i in range(1, M-1):
+    F = np.zeros(M)
+    for i in range(1, M-2):
         F[i:i+2] += elemental_load_vector(f, X[i], X[i+1])
+    F[2] += g0/(X[1]-X[0])
+    F[-2] += g1/(X[-1]-X[-2])
 
-    return F
-
+    return F[1:-1]

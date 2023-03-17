2805b684706ac6fcefa8880caa5c95435bba08bc
martinsw01 <martin.s.winther@gmail.com>
2023-03-17 15:22:13 +0100

    Use trapezoidal method

--- quadrature.py
+++ quadrature.py
@@ -1,12 +1,7 @@
-from scipy.integrate import quad  # Incredibly overkill?
-
-def quadrature_phi0(f, x0, x1) -> float:
-    def f_psi0(x):
-        return f(x) * (x-x0)/(x1-x0)
-    return quad(f_psi0, x0, x1)[0]
+# Trapezoid quadratures
+def quadrature_phi0(f, x0, x1):
+    return 0.5*f(x1) * (x1-x0)
 
 
-def quadrature_phi1(f, x0, x1) -> float:
-    def f_psi1(x):
-        return f(x) * (x-x1)/(x0-x1)
-    return quad(f_psi1, x0, x1)[0]
+def quadrature_phi1(f, x0, x1):
+    return 0.5*f(x0) * (x1-x0)

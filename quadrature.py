b0891f6f8d4e5fa0f4f97a289195b43cfd6ed4cb
martinsw01 <martin.s.winther@gmail.com>
2023-03-24 14:58:33 +0100

    Use gaussian quadrature

--- quadrature.py
+++ quadrature.py
@@ -1,7 +1,18 @@
 # Trapezoid quadratures
+import numpy as np
+
+
 def quadrature_phi0(f, x0, x1):
-    return 0.5*f(x1) * (x1-x0)
+    return gaussian_quad(lambda x: f(x)*(x-x0)/(x1-x0), x0, x1)
 
 
 def quadrature_phi1(f, x0, x1):
-    return 0.5*f(x0) * (x1-x0)
+    return gaussian_quad(lambda x: f(x)*(x1-x)/(x1-x0), x0, x1)
+
+
+ksi, w = np.polynomial.legendre.leggauss(2)
+
+
+def gaussian_quad(f, x0, x1):
+    q = 0.5*(x1-x0)*ksi+0.5*(x1+x0)
+    return 0.5*(x1-x0)*np.sum(w*f(q))

f021fe311e27288f1b006ec571c19bc2e948fd59
martinsw01 <martin.s.winther@gmail.com>
2023-03-24 14:57:35 +0100

    Make it possible to create load vectors for non-smooth solutions

--- load_vector.py
+++ load_vector.py
@@ -10,21 +10,20 @@ def elemental_load_vector(f, x0, x1):
     ])
 
 
-def load_vector(f, X, M, g0=0, g1=0):
+def load_vector(f, X, M, elemental_load_vector=elemental_load_vector, g0=0, g1=0):
     """
     :param f: Source
     :param X: Grid points from 0 to 1 of size M
+    :param elemental_load_vector: creates the elemental load vector for each element
     :param g0: boundary at x=0
     :param g1: boundary at x=1
     """
 
     F = np.zeros(M)
-    for i in range(1, M-2):
+    for i in range(M-1):
         F[i:i+2] += elemental_load_vector(f, X[i], X[i+1])
+
     F[2] += g0/(X[1]-X[0])
     F[-2] += g1/(X[-1]-X[-2])
 
     return F[1:-1]
-
-def load_vector_with_gradient(f, X, M, g0=0, g1=0):
-    pass
